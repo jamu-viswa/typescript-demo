b426305e07b271c2477ee258ca9b894fc04f04ee
JamunaDharshini <717822s120@kce.ac.in>
2026-03-06 14:00:45 +0530

    Stop tracking dist/ folder and compiled JS files

--- dist/Person.js
+++
@@ -1,9 +0,0 @@
-export class Person {
-    constructor(name, address) {
-        this.name = name;
-        this.address = address;
-    }
-    displayAddress() {
-        console.log("Address:", this.address);
-    }
-}
