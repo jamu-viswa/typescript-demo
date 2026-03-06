391d87d1e071522a030b3487a9c31d08078fcae5
jamu-viswa <jamuviswa.com@gmail.com>
2026-03-06 14:19:35 +0530

    Create Person class with name and address properties

---
+++ src/Person.ts
@@ -0,0 +1,14 @@
+
+export class Person {
+    name: string;
+    address: string;
+
+    constructor(name: string, address: string) {
+        this.name = name;
+        this.address = address;
+    }
+
+    displayAddress(): void {
+        console.log("Address:", this.address);
+    }
+}
